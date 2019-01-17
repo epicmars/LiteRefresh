31021f20eb34be5c40d983c3b6c2946818ddcb69
jastrelax <yinpinjiu@gmail.com>
2019-01-18 00:10:13 +0800

    Rename 'app' module to 'samples'

--- literefresh-widgets/src/main/java/com/androidpi/literefresh/widget/LoadingFooterView.java
+++ literefresh-widgets/src/main/java/com/androidpi/literefresh/widget/LoadingFooterView.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.androidpi.literefresh.sample.ui.widget;
+package com.androidpi.literefresh.widget;
 
 import android.content.Context;
 import android.support.annotation.Nullable;
